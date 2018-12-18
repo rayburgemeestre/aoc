43257c2ec570c39fbe799ea093bad7e7c6fa2bea
Ray Burgemeestre <ray.burgemeestre@brightcomputing.com>
2018-12-18 11:23:34 +0100

    Implement part 2 for Day 15 and calculate it with go routines in batches

--- 2018/15/main.go
+++ 2018/15/main.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"os"
 	"sort"
+	"sync"
 )
 
 type Unit struct {
@@ -25,14 +26,89 @@ type Tile struct {
 	cost int
 }
 
-var world [][]Tile
-var units []*Unit
-var sides []CoordCost
+var (
+	wg    sync.WaitGroup
+	sides []CoordCost
+)
+
+//var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
+	//flag.Parse()
+	//if *cpuprofile != "" {
+	//	f, err := os.Create(*cpuprofile)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	pprof.StartCPUProfile(f)
+	//}
+
+	// Part one
+	partOneAnswer, _ := solve("input", 3)
+	fmt.Println(partOneAnswer)
+	//pprof.StopCPUProfile()
+
+	// Part two
+	const maxConcurrentRoutines = 8
+	wg = sync.WaitGroup{}
+	type Result struct {
+		attack int
+		answer int
+		killed int
+	}
+	ret := make(chan Result)
+
+	var answer *Result
+	minFound := math.MaxInt32
+	for check := 4; ; {
+		// check batch
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < maxConcurrentRoutines; i++ {
+				res := <-ret
+				if res.killed == 0 {
+					fmt.Println("All elves survived with attack power:", res.attack)
+				}
+				if res.killed == 0 && res.attack < minFound {
+					minFound = res.attack
+					answer = &res
+				}
+			}
+		}()
+
+		// launch batch
+		for i := 0; i < maxConcurrentRoutines; i++ {
+			wg.Add(1)
+			go func(check int) {
+				defer wg.Done()
+				fmt.Println("Executing game with elves attack power of", check)
+				answer, elvesKilled := solve("input", check)
+				ret <- Result{check, answer, elvesKilled}
+			}(check)
+			check++
+		}
+		wg.Wait()
+		if minFound != math.MaxInt32 {
+			fmt.Println("Answer of part two:")
+			fmt.Println(answer.answer)
+			break
+		}
+	}
+}
+
+func solve(filename string, elves_attack_power int) (answer int, elvesKilled int) {
+
+	var (
+		world [][]Tile
+		units []*Unit
+	)
+
+	debug := false
 	units = []*Unit{}
 
-	readMap()
+	world = [][]Tile{}
+	readMap(filename, &world, &units)
 
 	sides = []CoordCost{
 		{0, -1, 0}, // up
@@ -41,15 +117,17 @@ func main() {
 		{0, +1, 0}, // down
 	}
 
-	fmt.Println("Round Initially:")
-	visualize(&world)
+	//	debug = true
+	if debug {
+		fmt.Println("Initial round:")
+		visualize(&world, &units)
+	}
 
 	for round := 0; ; round++ {
-		sortUnitsByCoord()
+		sortUnitsByCoord(&units)
 
 		// Move units
 		for _, attacker := range units {
-			fmt.Println("processing unit:", attacker.x, attacker.y)
 			if attacker.HP <= 0 {
 				continue
 			}
@@ -57,40 +135,36 @@ func main() {
 			// Find shortest path to a vulnerable side of a victim
 			min := math.MaxInt32
 			var nextMove CoordCost
-			hasAdjecentEnemy := false
-			var foundVictim *Unit
 			for _, victim := range units {
 				if victim.HP <= 0 || victim.c == attacker.c || victim.x == attacker.x && victim.y == attacker.y {
 					continue // cannot target dead unit, same team or self
 				}
-				checkAttackVector := func(x int, y int, h int) {
-					if x >= 0 && y >= 0 && y < len(world) && x < len((world)[y]) {
-						if world[y][x].c != '.' || world[y][x].unit != nil {
-							return
-						}
-						w, next := calculateDistance(world[attacker.y][attacker.x], world[y][x], &world)
-						if w > 0 && w < min {
-							min, nextMove = w, CoordCost{next.x, next.y, next.cost}
-							foundVictim = victim
-						}
-					}
-				}
 				for _, side := range sides {
-					checkAttackVector(victim.x+side.x, victim.y+side.y, victim.HP)
+					func(x int, y int, h int) {
+						if x >= 0 && y >= 0 && y < len(world) && x < len((world)[y]) {
+							if world[y][x].c != '.' || world[y][x].unit != nil {
+								return
+							}
+							w, next := calculateDistance(world[attacker.y][attacker.x], world[y][x], &world)
+							if w > 0 && w < min {
+								min, nextMove = w, CoordCost{next.x, next.y, next.cost}
+							}
+						}
+					}(victim.x+side.x, victim.y+side.y, victim.HP)
 				}
 			}
 
 			// Determine if we have an enemy adjecent to us
-			checkAdjecentEnemy := func(x int, y int, attacker rune) bool {
-				if x >= 0 && y >= 0 && y < len(world) && x < len((world)[y]) {
-					if world[y][x].unit != nil && world[y][x].unit.c != attacker && world[y][x].unit.HP > 0 {
-						return true
-					}
-				}
-				return false
-			}
+			hasAdjecentEnemy := false
 			for _, side := range sides {
-				if checkAdjecentEnemy(attacker.x+side.x, attacker.y+side.y, attacker.c) {
+				if func(x int, y int, attacker rune) bool {
+					if x >= 0 && y >= 0 && y < len(world) && x < len((world)[y]) {
+						if world[y][x].unit != nil && world[y][x].unit.c != attacker && world[y][x].unit.HP > 0 {
+							return true
+						}
+					}
+					return false
+				}(attacker.x+side.x, attacker.y+side.y, attacker.c) {
 					hasAdjecentEnemy = true
 				}
 			}
@@ -102,8 +176,6 @@ func main() {
 					panic("Already occupied")
 				}
 
-				fmt.Println(attacker.x, attacker.y, string(attacker.c), "move towards victim", foundVictim.x, foundVictim.y, "next step", nextMove.x, nextMove.y)
-
 				// Move the unit on the map
 				world[nextMove.y][nextMove.x].unit = world[attacker.y][attacker.x].unit
 				world[attacker.y][attacker.x].unit = nil
@@ -113,91 +185,94 @@ func main() {
 				attacker.y = nextMove.y
 			}
 
-			// sortUnitsByCoord()
-
-			// Calculate damages
-			//for _, attacker := range units {
-			//    if attacker.HP <= 0 {
-			//        continue
-			//    }
-
 			// For each possible attacker find the first victim with the lowest health
 			minFound := math.MaxInt32
 			var unit *Unit
-			findWithLowestHealth := func(x int, y int) {
-				if !(x >= 0 && y >= 0 && y < len(world) && x < len((world)[y])) {
-					return // outside of the map
-				}
-				if world[y][x].unit != nil {
-					if world[y][x].unit.HP <= 0 || world[y][x].unit.c == attacker.c {
-						return // unit dead or same team
-					}
-					if world[y][x].unit.HP < minFound {
-						minFound = world[y][x].unit.HP
-						unit = world[y][x].unit
-					}
-				}
-			}
 			for _, side := range sides {
-				findWithLowestHealth(attacker.x+side.x, attacker.y+side.y)
+				func(x int, y int) {
+					if !(x >= 0 && y >= 0 && y < len(world) && x < len((world)[y])) {
+						return // outside of the map
+					}
+					if world[y][x].unit != nil {
+						if world[y][x].unit.HP <= 0 || world[y][x].unit.c == attacker.c {
+							return // unit dead or same team
+						}
+						if world[y][x].unit.HP < minFound {
+							minFound = world[y][x].unit.HP
+							unit = world[y][x].unit
+						}
+					}
+				}(attacker.x+side.x, attacker.y+side.y)
 			}
 			// We found a victim, slay it!!
 			if unit != nil {
-				unit.HP -= 3
+				if attacker.c == 'E' {
+					unit.HP -= elves_attack_power
+				} else {
+					unit.HP -= 3
+				}
 				if unit.HP <= 0 {
+					if unit.c == 'E' {
+						elvesKilled++
+						// Small optimization
+						if elves_attack_power != 3 {
+							return // early exit!
+						}
+					}
 					world[unit.y][unit.x].unit = nil
 				}
 			}
 		}
-
-		fmt.Printf("After %d round(s)\n", round+1)
-		visualize(&world)
-
-		numGoblins := 0
-		numElves := 0
+		if debug {
+			fmt.Printf("After %d round(s)\n", round+1)
+			visualize(&world, &units)
+		}
+		goblins, elves := 0, 0
 		for _, u := range units {
 			if u.HP > 0 && u.c == 'G' {
-				numGoblins++
+				goblins++
 			} else if u.HP > 0 && u.c == 'E' {
-				numElves++
+				elves++
 			}
 		}
-		if numElves == 0 || numGoblins == 0 {
-			fmt.Println(round)
+		if elves == 0 || goblins == 0 {
 			sum := 0
 			for _, s := range units {
 				if s.HP > 0 {
 					sum += s.HP
 				}
 			}
-			fmt.Printf("Inside %d round(s)\n", round)
-			visualize(&world)
-			fmt.Println(sum)
-			fmt.Print(sum * round)
-			os.Exit(0)
+			if debug {
+				fmt.Printf("Inside round %d - cannot continue\n", round)
+				visualize(&world, &units)
+				fmt.Println(sum)
+				fmt.Print(sum * round)
+			}
+			answer = sum * round
+			return
 		}
 	}
+	return
 }
 
-func readMap() {
-	world = [][]Tile{}
+func readMap(filename string, world *[][]Tile, units *[]*Unit) {
 	y := 0
-	forEachLineInFile("input", func(s string) {
-		world = append(world, []Tile{})
+	forEachLineInFile(filename, func(s string) {
+		*world = append(*world, []Tile{})
 		x := 0
 		for _, c := range s {
 			switch c {
 			case '#':
-				world[y] = append(world[y], Tile{c, nil, x, y, 0})
+				(*world)[y] = append((*world)[y], Tile{c, nil, x, y, 0})
 			default:
 				var unit *Unit
 				if c == 'E' || c == 'G' {
-					units = append(units, &Unit{c, 200, x, y})
-					unit = units[len(units)-1]
+					*units = append(*units, &Unit{c, 200, x, y})
+					unit = (*units)[len(*units)-1]
 					unit.x, unit.y = x, y
 					unit.y = y
 				}
-				world[y] = append(world[y], Tile{'.', unit, x, y, 0})
+				(*world)[y] = append((*world)[y], Tile{'.', unit, x, y, 0})
 			}
 			x++
 		}
@@ -205,13 +280,13 @@ func readMap() {
 	})
 }
 
-func sortUnitsByCoord() {
+func sortUnitsByCoord(units *[]*Unit) {
 	// sort units
-	sort.Slice(units, func(i, j int) bool {
-		if units[i].y == units[j].y {
-			return units[i].x < units[j].x
+	sort.Slice(*units, func(i, j int) bool {
+		if (*units)[i].y == (*units)[j].y {
+			return (*units)[i].x < (*units)[j].x
 		}
-		return units[i].y < units[j].y
+		return (*units)[i].y < (*units)[j].y
 	})
 }
 
@@ -251,15 +326,14 @@ type CoordCost struct {
 }
 
 func calculateDistance(from Tile, to Tile, world *[][]Tile) (distance int, next CoordCost) {
-	exploredNodes := map[CoordCost]bool{}
+	explored := map[CoordCost]bool{}
 	frontier := &TileHeap{}
 	heap.Init(frontier)
-	from.cost = 0
 	heap.Push(frontier, from)
 	pred := map[CoordCost]CoordCost{}
 	for frontier.Len() != 0 {
-		currentp := &(*frontier)[0]
-		current := Tile{currentp.c, currentp.unit, currentp.x, currentp.y, currentp.cost}
+		p := &(*frontier)[0]
+		current := Tile{p.c, p.unit, p.x, p.y, p.cost}
 		heap.Pop(frontier)
 
 		if current.x == to.x && current.y == to.y {
@@ -277,42 +351,42 @@ func calculateDistance(from Tile, to Tile, world *[][]Tile) (distance int, next 
 			return current.cost, lookup
 		}
 
-		exploredNodes[CoordCost{current.x, current.y, -1}] = true
-
-		exploreNeighbour := func(x int, y int, cost int) bool {
-			// outside of map
-			if !(x >= 0 && y >= 0 && y < len(*world) && x < len((*world)[y])) {
-				return false
-			}
-			// neighbour is wall or other unit
-			neighbour := (*world)[y][x]
-			if neighbour.c == '#' || (neighbour.unit != nil && neighbour.unit != to.unit) {
-				return false
-			}
-			// already explored
-			if _, alreadyExplored := exploredNodes[CoordCost{neighbour.x, neighbour.y, -1}]; alreadyExplored {
-				return false
-			}
-
-			// find in frontier
-			foundInFrontier := false
-			for _, v := range *frontier {
-				if v.x == neighbour.x && v.y == neighbour.y {
-					foundInFrontier = true
-					break
-				}
-			}
-
-			if !foundInFrontier {
-				neighbour.cost = current.cost + cost
-				heap.Push(frontier, Tile{neighbour.c, neighbour.unit, neighbour.x, neighbour.y, neighbour.cost})
-				pred[CoordCost{neighbour.x, neighbour.y, neighbour.cost}] = CoordCost{current.x, current.y, current.cost}
-			}
-			return true
-		}
+		explored[CoordCost{current.x, current.y, -1}] = true
 
 		for _, side := range sides {
-			exploreNeighbour(current.x+side.x, current.y+side.y, 1)
+			func(x int, y int, cost int) {
+				// outside of map
+				if !(x >= 0 && y >= 0 && y < len(*world) && x < len((*world)[y])) {
+					return
+				}
+				// neighbour is wall or other unit
+				neighbour := (*world)[y][x]
+				if neighbour.c == '#' || (neighbour.unit != nil && neighbour.unit != to.unit) {
+					return
+				}
+				// already explored
+				if _, explored := explored[CoordCost{neighbour.x, neighbour.y, -1}]; explored {
+					return
+				}
+
+				// find in frontier
+				foundInFrontier := false
+				for _, v := range *frontier {
+					if v.x == neighbour.x && v.y == neighbour.y {
+						foundInFrontier = true
+						break
+					}
+				}
+
+				if !foundInFrontier {
+					neighbour.cost = current.cost + cost
+					heap.Push(frontier, neighbour)
+					neighbourCoordCost := CoordCost{neighbour.x, neighbour.y, neighbour.cost}
+					currentCoordCost := CoordCost{current.x, current.y, current.cost}
+					pred[neighbourCoordCost] = currentCoordCost
+				}
+				return
+			}(current.x+side.x, current.y+side.y, 1)
 		}
 	}
 	return
@@ -333,7 +407,7 @@ func forEachLineInFile(filename string, callback func(string)) {
 	}
 }
 
-func visualize(world *[][]Tile) {
+func visualize(world *[][]Tile, units *[]*Unit) {
 	for y := 0; y < len(*world); y++ {
 		for x := 0; x < len((*world)[y]); x++ {
 			if tile := (*world)[y][x]; tile.unit != nil {
@@ -344,7 +418,7 @@ func visualize(world *[][]Tile) {
 		}
 		fmt.Println()
 	}
-	for _, u := range units {
+	for _, u := range *units {
 		fmt.Println(u.x, u.y, string(u.c), u.HP)
 	}
 }
