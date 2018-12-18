b755a64b9c6722b3f2c1d679f2a260eaf9f31d5a
Ray Burgemeestre <ray.burgemeestre@brightcomputing.com>
2018-12-18 11:26:32 +0100

    Restore tests

--- 2018/15/main_test.go
+++ 2018/15/main_test.go
@@ -9,26 +9,30 @@ func TestExample(t *testing.T) {
 	}
 }
 
-//func TestExample2(t *testing.T) {
-//	if observed, expected := solve("input_test2", 3), 27828; observed != expected {
-//		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
-//	}
-//}
-//
-//func TestExample3(t *testing.T) {
-//	if observed, expected := solve("input_combat1", 3), 36334; observed != expected {
-//		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
-//	}
-//}
-//
-//func TestExample4(t *testing.T) {
-//	if observed, expected := solve("input_combat2", 3), 39514; observed != expected {
-//		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
-//	}
-//}
-//
-//func TestSolutionPart1(t *testing.T) {
-//	if observed, expected := solve("input", 3), 269430; observed != expected {
-//		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
-//	}
-//}
+func TestExample2(t *testing.T) {
+    observed, _ := solve("input_test2", 3)
+    if expected := 27828; observed != expected {
+		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
+	}
+}
+
+func TestExample3(t *testing.T) {
+	observed, _ := solve("input_combat1", 3)
+    if expected := 36334; observed != expected {
+		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
+	}
+}
+
+func TestExample4(t *testing.T) {
+	observed, _ := solve("input_combat2", 3)
+	if expected := 39514; observed != expected {
+		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
+	}
+}
+
+func TestSolutionPart1(t *testing.T) {
+	observed, _ := solve("input", 3)
+	if expected := 269430; observed != expected {
+		t.Errorf("Expected value was %d, got: %d.\n", expected, observed)
+	}
+}
